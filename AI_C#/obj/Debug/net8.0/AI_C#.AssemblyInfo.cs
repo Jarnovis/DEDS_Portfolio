82165c01a70c23cd24a0219356ffe28ebd6a80e8
Jarno Vis <84909095+Jarnovis@users.noreply.github.com>
2025-05-19 23:40:42 +0200

    Evaluatie en reflectie

--- AI_C#/obj/Debug/net8.0/AI_C#.AssemblyInfo.cs
+++ AI_C#/obj/Debug/net8.0/AI_C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI_C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f90ee265b779979a69a31d6973e5b0e3c1cc8fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+064621f0c665424d423965801840387fb50db714")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI_C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI_C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
